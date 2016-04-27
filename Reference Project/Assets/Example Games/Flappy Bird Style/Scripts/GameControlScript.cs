9bdeb3c228fb01dd33f0d4287deee17146217c76
kciam001 <kciam001@ucr.edu>
2016-04-26 17:38:50 -0700

    flappy bird

--- Reference Project/Assets/Example Games/Flappy Bird Style/Scripts/GameControlScript.cs
+++ Reference Project/Assets/Example Games/Flappy Bird Style/Scripts/GameControlScript.cs
@@ -6,9 +6,15 @@ public class GameControlScript : MonoBehaviour
 	public static GameControlScript current;	//a reference to our game control so we can access it statically
 	public ColumnSpawnScript columnSpawner;		//a reference to the column spawner
 	public GUIText scoreText;					//a reference to text that shows the player's score
+	public GUIText highScoreText;
 	public GameObject gameOvertext;				//a reference to the object that contains the text that appears when the player dies
 
-	int score = 0;								//the player's score
+	int score = 0;							//the player's score
+	int highScore = 0;
+	//if(PlayerPrefs.HasKey ("High Score")) {			//the player's highscore
+		//highScore = PlayerPrefs.GetInt ("High Score");
+	//}
+							
 	bool isGameOver = false;					//is the game over?
 
 
@@ -26,6 +32,10 @@ public class GameControlScript : MonoBehaviour
 
 	void Update()
 	{
+		if (PlayerPrefs.HasKey ("High Score"))
+		{
+			highScoreText.text = "High Score: " + PlayerPrefs.GetInt("High Score");
+		}
 		//if the game is over and the player has pressed some input...
 		if (isGameOver && Input.anyKey) 
 		{
@@ -37,12 +47,32 @@ public class GameControlScript : MonoBehaviour
 	public void BirdScored()
 	{
 		//the bird can't score if the game is over
-		if (isGameOver)	
+		if (isGameOver){
+			if(score > PlayerPrefs.GetInt("High Score"))
+				PlayerPrefs.SetInt ("High Score", score);
 			return;
+		}
 		//increase score
 		score++;
 		//adjust the score text
 		scoreText.text = "Score: " + score;
+		if (PlayerPrefs.HasKey ("High Score")) {
+			highScore = PlayerPrefs.GetInt ("High Score");
+			if (score > highScore) {
+				highScore++;
+				PlayerPrefs.SetInt ("High Score", highScore);
+				highScoreText.text = "High Score: " + highScore;
+			} else
+			{
+				highScoreText.text = "High Score: " + highScore;
+			}
+		} 
+		else
+		{
+			highScore = score;
+			PlayerPrefs.SetInt ("High Score", highScore);
+			highScoreText.text = "High Score: " + highScore;
+		}
 	}
 
 	public void BirdDied()
@@ -54,4 +84,8 @@ public class GameControlScript : MonoBehaviour
 		//set the game to be over
 		isGameOver = true;
 	}
+//	void updateScore()
+//	{
+//		highScoreText = "High score: " + highScore;
+//	}
 }
